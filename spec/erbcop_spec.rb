313e15d9deb416fb3d806d0f7c72259acf0bef42
Ryo Nakamura <r7kamura@gmail.com>
2022-01-15 03:15:45 +0900

    bundle gem erbcop

---
+++ spec/erbcop_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Erbcop do
+  it "has a version number" do
+    expect(Erbcop::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
